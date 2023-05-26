893a5af75e2e14121daaa3970493506bcc50f9fc
Karol Sadurski <100320522+K0rchard@users.noreply.github.com>
2023-05-26 22:12:11 +0200

    code cleanup & responsiveness

--- script.js
+++ script.js
@@ -54,36 +54,3 @@ function updateButtonVisibility() {
 // Event listeners for next and previous buttons
 document.querySelector(".nextBtn").addEventListener("click", nextSlide);
 document.querySelector(".prevBtn").addEventListener("click", prevSlide);
-
-// document.getElementById("nextBtn").addEventListener("click", nextSlide);
-// document.getElementById("prevBtn").addEventListener("click", prevSlide);
-
-
-
-
-
-
-// const rangeLabel = document.querySelector('.custom-range-slider');
-// const rangeInput = rangeLabel.children[0];
-// const thumbWidth = 6;
-
-// rangeLabel.insertAdjacentHTML(
-//   'beforeend', 
-//   `<span class="bubble"></span>`
-// );
-
-// const rangeBubble = rangeLabel.children[1];
-
-// positionBubble(rangeBubble, rangeInput)
-
-// function positionBubble(bubbleElement, anchorElement) {
-//   const {min, max, value, offsetWidth} = anchorElement;
-//   const total = Number(max) - Number(min);
-//   const perc = (Number(value) - Number(min)) / total;
-//   const offset = (thumbWidth/2) - (thumbWidth * perc);
-  
-//   bubbleElement.style.left = `calc(${perc * 100}% + ${offset}px)`;
-//   bubbleElement.textContent = value;
-// }
-
-// rangeInput.addEventListener('input', (e) => positionBubble(rangeBubble, e.target))
